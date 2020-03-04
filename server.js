773eeca86f87b72420d54e1f8e18bbfe2308286a
GitOffMyCode <gennaheald@outlook.com>
2020-03-04 14:33:31 +0000

    server amended

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const pledgeData = require("./pledges.json");
+
+const app = express();
+app.use(cors());
+
+app.get("/pledges", (req, res) => {
+  res.json(pledgeData);
+} )
+
+app.listen(3000, (err) => {
+  if (err) {
+      console.log("there was an error", err)
+      return;
+  };
+  console.log("now listening on port 3000");
+});
+
+console.log("hello! test")
