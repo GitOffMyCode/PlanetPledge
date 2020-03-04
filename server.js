6ee35eb64e4bc8934b62c29dbed1346def074101
GitOffMyCode <gennaheald@outlook.com>
2020-03-04 17:26:01 +0000

    package.json amended to move start into scripts

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const pledgeData = require("./pledges.json");
 const app = express();
 app.use(cors());
 
+// url: http://localhost:3000/pledges
 app.get("/pledges", (req, res) => {
+  console.log(req.headers)
   res.json(pledgeData);
-} )
+})
+
 
 module.exports = app;
--- server.test.js
+++ server.test.js
@@ -1,7 +1,7 @@
 const app = require("./server.js");
 const supertest = require("supertest");
 
-describe("test API",() => {
+describe("test API", () => {
     test("returns status 200 when api called", () => {
         return supertest(app)
         .get("/pledges")
