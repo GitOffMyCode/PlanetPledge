689a0d60fd9fd950726061a80f8021ccd1a51149
GitOffMyCode <genevieve.heald@infinityworks.com>
2020-03-09 13:28:26 +0000

    get by id test written and tested

--- server.test.js
+++ server.test.js
@@ -1,12 +1,31 @@
 const app = require("./server");
 const supertest = require("supertest");
 
-describe("test API", () => {
+describe("api", () => {
   test("returns status 200 when api called", () => {
-        return supertest(app)
-            .get("/pledges")
-            .expect(200);
-    })
+    return supertest(app)
+      .get("/pledges")
+      .expect('Content-Type', /json/)
+      .expect(200)
+  })
+
+  test("returns status 200 when api called to get a pledge by pledge_id", () => {
+    const pledge_id_2 = [{
+      pledge_title: 'cycle to work',
+      pledge_detail: 'instead of driving to save planet',
+      pledge_type: 'D',
+      pledge_id: 2,
+      username: 'HelenG'
+    }]
+    return supertest(app)
+      .get("/pledges/2")
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(res => {
+        console.log(res.body)
+        expect(res.body).toStrictEqual(pledge_id_2)
+      });
+  })
 })
 
 
