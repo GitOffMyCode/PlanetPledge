5c4d41ac45b94b02020eabc69cdaa84e8c583af2
GitOffMyCode <genevieve.heald@infinityworks.com>
2020-03-09 14:13:08 +0000

    delete test completed and delete endpoint added and passing test

--- server.test.js
+++ server.test.js
@@ -2,6 +2,7 @@ const app = require("./server");
 const supertest = require("supertest");
 
 describe("api", () => {
+
   test("returns status 200 when api called", () => {
     return supertest(app)
       .get("/pledges")
@@ -22,9 +23,16 @@ describe("api", () => {
       .expect('Content-Type', /json/)
       .expect(200)
       .then(res => {
-        console.log(res.body)
+        // console.log(res.body)
         expect(res.body).toStrictEqual(pledge_id_2)
       });
+  })
+
+  test("deletes a pledge when passed pledge_id as a parameter", () => {
+    return supertest(app)
+      .delete("/pledges/8")
+      // .expect('user deleted')
+      .expect(200)
   })
 })
 
