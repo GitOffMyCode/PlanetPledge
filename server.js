ea6ceae6f6e5f298fcb4ce2960573f23632bb4ff
GitOffMyCode <genevieve.heald@infinityworks.com>
2020-03-11 15:31:50 +0000

    endpoints amended

--- server.js
+++ server.js
@@ -6,25 +6,25 @@ app.use(express.json()); //Used to parse JSON bodies
 app.use(cors());
 
 
-// url: http://localhost:3000/pledges
+// url: http://localhost:3002/pledges
 app.get('/pledges', db.getPledges);
 
-// url: http://localhost:3000/pledges/{id}
+// url: http://localhost:3002/pledges/{id}
 app.get('/pledges/:id', db.getPledgeById);
 
-// url: http://localhost:3000/pledges/{id}
+// url: http://localhost:3002/pledges_status
 app.get('/pledges_status', db.getPledgeStatus);
 
-// url: http://localhost:3000/pledges
+// url: http://localhost:3002/pledges
 app.post('/pledges', db.postPledge);
 
-// url: http://localhost:3000/pledges/{id}
+// url: http://localhost:3002/pledges/{id}
 app.delete('/pledges/:id', db.deletePledge);
 
-// url: http://localhost:3000/pledges{id}
+// url: http://localhost:3002/pledges{id}
 app.post('/pledges_status/:id', db.markDailyPledgeAsActive);
 
-// url: http://localhost:3000/pledges{id}
+// url: http://localhost:3002/pledges{id}
 app.delete('/pledges_status/:id', db.markDailyPledgeAsInactive);
 
 module.exports = app;
